6e6115c644c646d4dda0fc051733124a51181229
Ilya Lavrinov <iilavr@mail.ru>
2023-08-18 07:36:15 +0400

    Initial Commit

---
+++ EatWork/EatWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatWorkApp.swift
+//  EatWork
+//
+//  Created by Илья on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct EatWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
